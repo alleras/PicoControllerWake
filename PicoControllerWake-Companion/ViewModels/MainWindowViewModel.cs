d9dee8de0ed36b194274a2f0dc2a44f3378018fe
Agustin Lleras <agustin.lleras@indysoft.com>
2025-10-15 00:21:06 -0400

    Remove warnings for ViewModel design-time constructors (TODO: Create separate design-time view models)

--- PicoControllerWake-Companion/ViewModels/MainWindowViewModel.cs
+++ PicoControllerWake-Companion/ViewModels/MainWindowViewModel.cs
@@ -19,7 +19,9 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private bool _initialized = false;
     public ObservableCollection<BluetoothDeviceViewModel> BluetoothDeviceViews { get; } = [];
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     public MainWindowViewModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
         BluetoothDeviceViews.Add(new BluetoothDeviceViewModel());
         BluetoothDeviceViews.Add(new BluetoothDeviceViewModel());
